7817d1c439e0b95436c00ac03bcfd1bf120a2f10
Holden Green <holdenmgreen@gmail.com>
2022-03-10 00:04:14 -0800

    think I'm moving to gitee

---
+++ readme.md
@@ -0,0 +1,3 @@
+# GEMx
+X layer of the system.
+This is the process that creates the environment and exposes interfaces.
---
+++ src/main.cpp
@@ -0,0 +1,11 @@
+/* Holden Green @ GEM and contributers.
+   Licensed under the Peer Production License. */
+
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    cout << "Hello, this is the GEMx pseudo kernel." << endl;
+}
